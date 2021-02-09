a4b6f8abf3dd5c0c4063eb245acab3b14b407436
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2021-02-09 17:08:36 +0100

    exercises done.

--- 01_intro_ggplot2.R
+++ 01_intro_ggplot2.R
@@ -108,19 +108,39 @@ ggplot(data = mpg) +
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy), color = "blue")
 
-# 2.  Which variables in 'mpg' are categorical?
-# manufacturer, model, trans, drv, fl, class
-#     Which variables are continuous?
-# displ, year, cty, hwy
-#     How can you see this information when you run 'mpg'?
-# data type below the column name.
+# 2.  Q: Which variables in 'mpg' are categorical?
+#     A: manufacturer, model, trans, drv, fl, class
+#     Q: Which variables are continuous?
+#     A: displ, year, cty, hwy
+#     Q: How can you see this information when you run 'mpg'?
+#     A: The data type is visible right below the column name.
 
-# 3. Map a contiuous variable to 'color', 'size' and 'shape'.
-#     How do these aesthetics behave differently for categorical versus
-#     continuous variables?
-
+# 3. Map a continuous variable to 'color', 'size' and 'shape'.
+# color:
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, color = cyl))
+# size:
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, size = cyl))
+# shape:
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, shape = as.character(cyl)))
+#     Q: How do these aesthetics behave differently for categorical versus
+#         continuous variables?
+#     A: 'shape' raises an error using a continuous variable, but works when
+#         converted to a character as long as there are only few different
+#         values.
 
 # 4. What happens if you map the same variable to multiple aesthetics?
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, color = cty, size = cty))
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, color = cyl, shape = as.character(cyl)))
+#     A: Works - both aesthetics are modified.
 
 # 5. What does the 'stroke' aesthetic do? What shapes does it work with?
 #     (Hint: use ?geom_point)
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, stroke = cyl), shape = 21,
+             fill = 'red', size = 4, color = 'white')
+#     A: 'stroke' surrounds the points with a yard or lift.
